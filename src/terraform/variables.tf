b63a9bca73df7d7588af0a1decc68c09267f66f2
rodrigowafonso <rodrigowalterafonso@gmail.com>
2025-01-28 16:17:00 -0300

    Provisionando a infraestrutura com Terraform

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -1,13 +1,3 @@
-variable "appid_azure" {
-  type        = string
-  description = "Define o ID do Service Principal"
-}
-
-variable "apppassword_azure" {
-  type        = string
-  description = "Define a Password do Service Principal"
-}
-
 variable "regiao" {
   type = string
   default = "eastus"
